a9c48c6141927890b97e7a08d31caa73a421d4fe
acoffey21 <amelia.coffey@tufts.edu>
2018-10-14 16:10:32 -0400

    added javascript into index for debugging purposes, added stylesheet, added correct api key

--- mbta/mapscript.js
+++ mbta/mapscript.js
@@ -1,9 +1,32 @@
-var map;
-	function drawMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-        	center: {lat: 42.39674, lng: -71.121815},
-        	zoom: 8
-        });
-      }
+	function init() {
+		// south station
+		var southstation = new google.maps.LatLng(42.352271, -71.05524200000001);
 
-src="https://maps.googleapis.com/maps/api/js?key=?------&callback=initMap" async defer>
+		// set up map
+		var options = { 
+			zoom : 13,
+			center: southstation,
+			mapTypeId: google.maps.mapTypeId.ROADMAP
+		};
+
+		// create map
+        var map = new google.maps.Map(document.getElementById('map'), options);
+
+        //create marker
+        var marker = new google.maps.Marker({
+        	position: landmark,
+        	title: "South Station"
+        	//add image
+        	//add schedule info
+        });
+        marker.setMap(map);
+
+        //global info window ***
+        var infowindow = new google.maps.InfoWindow();
+
+        // open info window on click
+        google.maps.event.addListener(marker, 'click', function () {
+        	infowindow.setContent(marker.-----);
+        	infowindow.open(map,marker);
+        });
+}
